5a60433708e205969af3e937406f06098d216e3a
alpheratzz <alpheratz93@gmail.com>
2017-05-17 13:50:53 +0300

    First commit

---
+++ OAuthThing.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace RandomThings
+{
+    class OAuthThing
+    {
+        HttpClient client;
+
+        string consumerKey;
+        string nonce;
+        string signature;
+        string signatureMethod;
+        string timestamp;
+        string oauthToken;
+        string oauthVersion;
+
+        public OAuthThing()
+        {
+            client = new HttpClient();
+
+            signatureMethod = "HMAC_SHA1";
+            oauthVersion = "1.0";
+            nonce = Guid.NewGuid().ToString("N");
+            timestamp = (DateTime.Now - new DateTime(1970, 1, 1)).TotalSeconds.ToString();
+        }
+
+        void getSignature(string httpMethod)
+        {
+            HMACSHA1 hmac = new HMACSHA1();
+            string key = PercentEncode("key");
+            byte[] keyBytes = Encoding.ASCII.GetBytes(key);
+            hmac.ComputeHash(Encoding.ASCII.GetBytes(PercentEncode("POST&https://hello.com&text=aaa")));
+            //hmac.Key = ...;
+        }
+
+        void getSignature(HttpMethod method, string requestUrl)
+        {
+            client.BaseAddress = new Uri("");
+            client.DefaultRequestHeaders.Add("a", "b");
+
+            HttpRequestMessage message = new HttpRequestMessage(method, "request/uri");
+        }
+
+        public static string PercentEncode(string something)
+        {
+            return Uri.EscapeDataString(something);
+        }
+    }
+}
---
+++ Tweeter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomThings
+{
+    class Tweeter
+    {
+        public Tweeter()
+        { }
+
+        public void Tweet(string text)
+        { }
+
+        static int GetUnixTimestamp(DateTime utcDate)
+        {
+            return (int)(utcDate - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds;
+        }
+
+        static string PercentEncode(string text)
+        {
+            return Uri.EscapeDataString(text);
+        }
+    }    
+}
